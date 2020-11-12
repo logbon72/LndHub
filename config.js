da05b81943235a29ae10deb24034cbdc0476c34e
Taiwo Orilogbon <taiwo@bundle.africa>
2020-11-12 14:49:55 +0100

    Make Fees configurable

--- config.js
+++ config.js
@@ -2,9 +2,10 @@ let config = {
   redisUri: process.env.REDIS_URI || 'redis://127.0.0.1:6379',
   lnd: {
     url: process.env.LND_URL || '127.0.0.1:10009',
-    password: process.env.LND_URL || '',
+    password: process.env.LND_PASSWORD || '',
   },
   groundControlUrl: process.env.GROUNDCONTROL,
+  feesPercent: Number(process.env.FEES_PERCENT || 0),
 };
 
 if (process.env.CONFIG) {
